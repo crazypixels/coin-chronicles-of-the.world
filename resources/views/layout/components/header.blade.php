c8867bf55e4b2c1253cf623a1789b55520ce0413
Jonathan De Jans <jonathan.de.jans@nxtpeople.com>
2025-05-22 22:19:18 +0200

    WIP

--- resources/views/layout/components/header.blade.php
+++ resources/views/layout/components/header.blade.php
@@ -1,4 +1,24 @@
 
 <header>
-
+    <div class="container">
+        <div class="top-bar">
+            <div class="logo">
+                <img src="{{ asset('images/coin-chronicles.png') }}" alt="Logo">
+            </div>
+            <div class="search-bar">
+                <input type="text" placeholder="Zoeken...">
+            </div>
+            <div class="user-info">
+                
+            </div>
+        </div>
+    </div>
 </header>
+
+<nav>
+    <div class="container">
+        <a href="">Munten</a>
+        <a href="">Heersers</a>
+        <a href="">Landen</a>
+    </div>
+</nav>
